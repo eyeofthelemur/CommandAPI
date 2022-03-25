2f8eee9ae91efe6faa0fc0749079b802d7aa276a
eyeofthelemur <lemureye999@gmail.com>
2022-03-25 15:23:57 -0500

    Added CommandContext for data repository

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -15,13 +15,6 @@ namespace CommandAPI.Controllers
             _repository = repository;
         }
 
-        //[HttpGet]
-        //public ActionResult<IEnumerable<string>> Get()
-        //{
-
-            //return new string[] { "this", "is", "hard", "coded" };
-        //}
-
         [HttpGet]
         public ActionResult<IEnumerable<Command>> GetAllCommands()
         {
--- src/CommandAPI/Data/ICommandAPIRepo.cs
+++ src/CommandAPI/Data/ICommandAPIRepo.cs
@@ -8,11 +8,8 @@ namespace CommandAPI.Data
         bool SaveChanges();
         IEnumerable<Command> GetAllCommands();
         Command GetCommandById(int id);
-
         void CreateCommand(Command cmd);
-
         void UpdateCommand(Command cmd);
-
         void DeleteCommand(Command cmd);
     }
 
