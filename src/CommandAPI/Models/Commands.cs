715fdb2bddadc335f2d50c30cc7f66c6bd1b2747
eyeofthelemur <lemureye999@gmail.com>
2022-03-25 08:56:03 -0500

    Added Command Model to API project. Adding a second comment since the first push did not work.

---
+++ src/CommandAPI/Models/Commands.cs
@@ -0,0 +1,17 @@
+namespace CommandAPI.Models
+{
+    public class CommandAPI
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength]
+        public string HowTo { get; set; }
+        [Required]
+        public string Platform { get; set; }
+        [Required]
+        public string CommandLine { get; set; }
+
+    }
+}
